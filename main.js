439ea7f8c33772aba2e53aa4201ca13541764a62
D-Ortea <sflock1@gmail.com>
2018-04-29 22:12:10 +0200

    Add display clearing and delete functions

--- main.js
+++ main.js
@@ -2,21 +2,55 @@ const expression = document.querySelector(`div[class="expression"]`);
 const numInput = document.querySelector(`div[class="num-input"`);
 const keypad = document.querySelector(`div[class="keypad"`);
 
+let deleteInput = true;
+
+
 function addNumberKeyListeners() {
-  const numKeys = keypad.querySelectorAll(`.num`);
+  const numKeys = keypad.querySelectorAll('.num');
   numKeys.forEach(numKey => {
     numKey.addEventListener('click', () => {
+      if (deleteInput) {
+        numInput.textContent = '';
+        deleteInput = false;
+      }
       numInput.textContent += numKey.value;
     });
   });
 }
 
-function resolveFn() {
+function addFnListeners() {
+  const fnKeys = keypad.querySelectorAll('.fn');
+  fnKeys.forEach(fnKey => {
+    fnKey.addEventListener('click', () => resolveFn(fnKey.value));
+  });
+}
+
+function addOpListeners() {
+
+}
+
+function resolveFn(key) {
+  window[key]();
+}
+
+function deleteOne() {
+  const test = numInput.textContent.slice(0, -1);
   
+  if(test) {
+    numInput.textContent = test;
+  } else {
+    clearInput();
+  }
 }
 
 function clearInput() {
-  numInput.textContent = '';
+  numInput.textContent = '0';
+  deleteInput = true;
+}
+
+function clearAll() {
+  expression.textContent = '';
+  clearInput();
 }
 
 function test() {
@@ -26,3 +60,4 @@ function test() {
 
 test();
 addNumberKeyListeners();
+addFnListeners();
